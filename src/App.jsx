c6307d0753f751f5699a1be0d3d47457aebf6c2b
Ghiurutan-Bura Péter <50810629+gburapeter@users.noreply.github.com>
2024-06-03 09:48:37 +0200

    add new changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,25 @@ import { getProject } from "@theatre/core";
 import studio from "@theatre/studio";
 import extension from "@theatre/r3f/dist/extension";
 import { OrthographicCamera, SheetProvider } from "@theatre/r3f";
+import demoProjectState from "./getstate.json";
 
+import { useEffect } from "react";
 studio.initialize();
 studio.extend(extension);
 // our Theatre.js project sheet, we'll use this later
-const demoSheet = getProject("Demo Project").sheet("Demo Sheet");
+// const demoSheet = getProject("Demo Project").sheet("Demo Sheet");
+const demoSheet = getProject("Demo Project", { state: demoProjectState }).sheet(
+	"Demo Sheet"
+);
+console.log(demoSheet);
 function App() {
 	// useEffect(() => {
 	// 	demoSheet.project.ready.then(() =>
 	// 		demoSheet.sequence.play({ iterationCount: 1, range: [0, 4.36] })
 	// 	);
+
 	// }, []);
+
 	return (
 		<>
 			<Canvas flat>
