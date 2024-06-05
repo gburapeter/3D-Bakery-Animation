448d16eb894520a52d383841fdba8b3b5a79a739
Ghiurutan-Bura Péter <50810629+gburapeter@users.noreply.github.com>
2024-06-05 14:15:37 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
 		demoSheet.project.ready.then(() => {
 			setTimeout(() => {
 				console.log("Delayed for 1 second.");
-			}, "1000");
+			}, "5000");
 			demoSheet.sequence.play();
 		});
 	}, []);
