d64652cb5c43a949bcce0bde298b416d410b7676
Ghiurutan-Bura Péter <50810629+gburapeter@users.noreply.github.com>
2024-06-05 15:41:56 +0200

    add new

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { getProject } from "@theatre/core";
 import studio from "@theatre/studio";
 import extension from "@theatre/r3f/dist/extension";
 import { OrthographicCamera, SheetProvider } from "@theatre/r3f";
-import demoProjectState from "./usethis.json";
+import demoProjectState from "./fixed.json";
 
 import { useEffect } from "react";
 if (import.meta.env.DEV) {
@@ -22,7 +22,6 @@ if (import.meta.env.DEV) {
 const demoSheet = getProject("Demo Project", { state: demoProjectState }).sheet(
 	"Demo Sheet"
 );
-console.log(demoSheet);
 function App() {
 	useEffect(() => {
 		demoSheet.project.ready.then(() => {
