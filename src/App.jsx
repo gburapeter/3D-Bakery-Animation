b59d06235ec2a53af668ea9a04d43c20419479b5
Ghiurutan-Bura Péter <50810629+gburapeter@users.noreply.github.com>
2024-06-17 14:45:42 +0200

    timeout

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import demoProjectState from "./fixedv2.json";
 // import extension from "@theatre/r3f/dist/extension";
 // import studio from "@theatre/studio";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Donut from "./Donut";
 import { Overlay } from "./Overlay";
 
@@ -23,7 +23,11 @@ const demoSheet = getProject("Demo Project", { state: demoProjectState }).sheet(
 	"Demo Sheet"
 );
 function App() {
-	useEffect(() => {
+	const todaysDate = new Date().toLocaleString();
+	const [showCanvas, setShowCanvas] = useState(false);
+
+	const handleStart = () => {
+		setShowCanvas(true);
 		demoSheet.project.ready.then(() => {
 			setTimeout(() => {
 				demoSheet.sequence
@@ -33,59 +37,63 @@ function App() {
 					.then(() => {
 						demoSheet.sequence.play();
 					});
-			}, 5000);
+			}, 600);
 		});
-	}, []);
-
-	const todaysDate = new Date().toLocaleString();
-
+	};
 	return (
 		<>
-			<div className="h-screen grid items-start py-16 px-6 lg:px-16 xl:px-32 2xl:px-48 grid-cols-4 w-screen relative">
-				<div className="col-span-2 text-4xl">Ghiurutan-Bura Peter</div>
+			{!showCanvas && (
+				<div className="h-screen grid items-start py-16 px-6 lg:px-16 xl:px-32 2xl:px-48 grid-cols-4 w-screen relative">
+					<div className="col-span-2 text-3xl lg:text-4xl">
+						Ghiurutan-Bura Peter
+					</div>
 
-				<div className="col-span-2 text-end flex flex-col space-y-8 text-3xl ">
-					<div>
+					<div className="col-span-2 text-end flex flex-col space-y-4 lg:space-y-8 text-xl lg:text-3xl z-50">
+						<div>
+							<a
+								target="_blank"
+								href="https://www.linkedin.com/in/p%C3%A9ter-ghiurutan-bura-2765a41a2/"
+								className="w-full relative group cursor-pointer hover:text-gray-700  "
+							>
+								Linkedin
+								<span className="absolute -bottom-1 right-0 w-full h-[0.15rem] bg-black transition-all group-hover:w-0"></span>
+							</a>
+						</div>
+						<div>
+							<a
+								target="_blank"
+								href="https://github.com/gburapeter"
+								className="w-full relative group cursor-pointer hover:text-gray-700  "
+							>
+								Github
+								<span className="absolute -bottom-1 right-0 w-full h-[0.15rem] bg-black transition-all group-hover:w-0"></span>
+							</a>
+						</div>
+					</div>
+
+					<div className="  col-span-2 text-lg inline-flex h-full items-end text-start">
 						<a
 							target="_blank"
-							href="https://www.linkedin.com/in/p%C3%A9ter-ghiurutan-bura-2765a41a2/"
-							className="w-full relative group cursor-pointer hover:text-gray-700  "
+							className=""
+							href="https://www.freepik.com/free-vector/hand-drawn-plain-donut_16263485.htm#query=donut%20svg&position=0&from_view=keyword&track=ais_user&uuid=997c96f7-11bd-4a70-8bea-4ec6211fe314"
 						>
-							Linkedin
-							<span className="absolute -bottom-1 right-0 w-full h-[0.15rem] bg-black transition-all group-hover:w-0"></span>
+							svg by{" "}
+							<span className="italic text-slate-900">rawpixel.com</span>
 						</a>
 					</div>
-					<div>
-						<a
-							target="_blank"
-							href="https://github.com/gburapeter"
-							className="w-full relative group cursor-pointer hover:text-gray-700  "
-						>
-							Github
-							<span className="absolute -bottom-1 right-0 w-full h-[0.15rem] bg-black transition-all group-hover:w-0"></span>
-						</a>
+
+					<div className=" h-full col-start-4 text-end inline-flex items-end justify-end w-full text-xl lg:text-3xl ">
+						{todaysDate}
+					</div>
+
+					<div className="fixed inset-0 w-full h-full justify-center inline-flex ">
+						<Donut />
+						<button className=" hover:bg-red-500" onClick={handleStart}>
+							start
+						</button>
 					</div>
 				</div>
-
-				<div className="  col-span-2 text-lg inline-flex h-full items-end text-start">
-					<a
-						target="_blank"
-						className=""
-						href="https://www.freepik.com/free-vector/hand-drawn-plain-donut_16263485.htm#query=donut%20svg&position=0&from_view=keyword&track=ais_user&uuid=997c96f7-11bd-4a70-8bea-4ec6211fe314"
-					>
-						svg by{" "}
-						<span className="italic text-slate-900">rawpixel.com</span>
-					</a>
-				</div>
-
-				<div className=" h-full col-start-4 text-end inline-flex items-end justify-end w-full text-3xl ">
-					{todaysDate}
-				</div>
-
-				<div className="fixed inset-0 w-full h-full justify-center inline-flex -z-10">
-					<Donut />
-				</div>
-			</div>
+			)}
 			<Canvas flat>
 				<SheetProvider sheet={demoSheet}>
 					<OrthographicCamera
