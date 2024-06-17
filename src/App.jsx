b6f8e8504c97a4a8f74afcbc0e38a4658bfbbc24
Ghiurutan-Bura Péter <50810629+gburapeter@users.noreply.github.com>
2024-06-17 14:04:23 +0200

    initial overlay

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { OrbitControls, PresentationControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { getProject } from "@theatre/core";
 import { OrthographicCamera, SheetProvider } from "@theatre/r3f";
-import extension from "@theatre/r3f/dist/extension";
-import studio from "@theatre/studio";
 import BakeryMusic from "../src/assets/cute-bakery-music.mp3";
 import "./App.css";
 import { Bakery } from "./Bakery";
 import demoProjectState from "./fixedv2.json";
+// import extension from "@theatre/r3f/dist/extension";
+// import studio from "@theatre/studio";
 
 import { useEffect } from "react";
-if (import.meta.env.DEV) {
-	studio.initialize();
-	studio.extend(extension);
-}
+import Donut from "./Donut";
+import { Overlay } from "./Overlay";
+// if (import.meta.env.DEV) {
+// 	studio.initialize();
+// 	studio.extend(extension);
+// }
 // our Theatre.js project sheet, we'll use this later
 // const demoSheet = getProject("Demo Project").sheet("Demo Sheet");
 const demoSheet = getProject("Demo Project", { state: demoProjectState }).sheet(
@@ -36,6 +38,22 @@ function App() {
 
 	return (
 		<>
+			<div className="h-screen grid items-start py-16 px-32 grid-cols-4 w-screen relative">
+				<div className="col-span-2 text-4xl">Ghiurutan-Bura Peter</div>
+
+				<div className="col-span-2 text-end flex flex-col space-y-8 text-3xl ">
+					<div className="cursor-pointer underline underline-offset-8 ">
+						Linkedin
+					</div>
+					<div className="cursor-pointer underline underline-offset-8 ">
+						Github
+					</div>
+				</div>
+
+				<div className="fixed inset-0 w-full h-full justify-center inline-flex">
+					<Donut />
+				</div>
+			</div>
 			<Canvas flat>
 				<SheetProvider sheet={demoSheet}>
 					<OrthographicCamera
