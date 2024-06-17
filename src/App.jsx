deaa5640de3258ae5cab9096f0e46314497973ff
Ghiurutan-Bura Péter <50810629+gburapeter@users.noreply.github.com>
2024-06-17 14:24:44 +0200

    overlay

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import demoProjectState from "./fixedv2.json";
 import { useEffect } from "react";
 import Donut from "./Donut";
 import { Overlay } from "./Overlay";
+
 // if (import.meta.env.DEV) {
 // 	studio.initialize();
 // 	studio.extend(extension);
@@ -36,21 +37,50 @@ function App() {
 		});
 	}, []);
 
+	const todaysDate = new Date().toLocaleString();
+
 	return (
 		<>
 			<div className="h-screen grid items-start py-16 px-32 grid-cols-4 w-screen relative">
 				<div className="col-span-2 text-4xl">Ghiurutan-Bura Peter</div>
 
 				<div className="col-span-2 text-end flex flex-col space-y-8 text-3xl ">
-					<div className="cursor-pointer underline underline-offset-8 ">
-						Linkedin
+					<div>
+						<a
+							target="_blank"
+							className="w-full relative group cursor-pointer hover:text-gray-700  "
+						>
+							Linkedin
+							<span className="absolute -bottom-1 right-0 w-full h-[0.15rem] bg-black transition-all group-hover:w-0"></span>
+						</a>
 					</div>
-					<div className="cursor-pointer underline underline-offset-8 ">
-						Github
+					<div>
+						<a
+							target="_blank"
+							className="w-full relative group cursor-pointer hover:text-gray-700  "
+						>
+							Github
+							<span className="absolute -bottom-1 right-0 w-full h-[0.15rem] bg-black transition-all group-hover:w-0"></span>
+						</a>
 					</div>
 				</div>
 
-				<div className="fixed inset-0 w-full h-full justify-center inline-flex">
+				<div className="  col-span-2 text-2xl inline-flex h-full items-end text-start">
+					<a
+						target="_blank"
+						className=""
+						href="https://www.freepik.com/free-vector/hand-drawn-plain-donut_16263485.htm#query=donut%20svg&position=0&from_view=keyword&track=ais_user&uuid=997c96f7-11bd-4a70-8bea-4ec6211fe314"
+					>
+						SVG by{" "}
+						<span className="italic text-slate-900">rawpixel.com</span>
+					</a>
+				</div>
+
+				<div className=" h-full col-start-4 text-end inline-flex items-end justify-end w-full text-3xl ">
+					{todaysDate}
+				</div>
+
+				<div className="fixed inset-0 w-full h-full justify-center inline-flex -z-10">
 					<Donut />
 				</div>
 			</div>
